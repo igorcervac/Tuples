995f996338891054459b0df68829acb6cf15eb54
Igor Cervac <igor.cervac@outlook.com>
2023-07-14 16:09:44 -0400

    Add project files.

---
+++ TuplesApp/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace TuplesApp
+{
+    internal class Car
+    {
+        public string Company { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;   
+        public int Year { get; set; }
+    }
+}
---
+++ TuplesApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+using TuplesApp;
+
+Console.WriteLine("Tuple of values");
+var tuple = (1, 100.00m, "test");
+Console.WriteLine(tuple.Item1);
+Console.WriteLine(tuple.Item2);
+Console.WriteLine(tuple.Item3);
+
+Console.WriteLine("Tuple of object values");
+var car = new Car { Company = "Ford", Model = "Explorer", Year = 2020 };
+var carTuple = (car.Company, car.Model, car.Year);
+Console.WriteLine(carTuple.Company);
+Console.WriteLine(carTuple.Model);
+Console.WriteLine(carTuple.Year);
+
+
+var carTupleAsJson = JsonSerializer.Serialize(carTuple, options: new JsonSerializerOptions { IncludeFields = true });
+Console.WriteLine("Tuple as json: {0}", carTupleAsJson);
+
